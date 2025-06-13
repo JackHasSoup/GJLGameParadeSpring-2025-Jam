b2620ebb5cccc7b4e105aa5aabfd2b8816221093
Ross McCorkindale <ross.mccorkindale30@gmail.com>
2025-06-13 20:53:21 +0100

    player size

--- Coursework/CMP105App/Player.cpp
+++ Coursework/CMP105App/Player.cpp
@@ -3,11 +3,20 @@
 Player::Player()
 {
 	//sealTexture.loadFromFile("gfx/sealTexture.png"); //Doesn't exist yet
-	
+	setSize(sf::Vector2f(50, 50));
+	setPosition(100, 100);
 
+	//availableActions = {
+	//	new BufferedCommand(&player, [](PhysicsObject* target) {target->accelerate({ 0.f, -35000.f }); }),//up
+	//	new BufferedCommand(&player, [](PhysicsObject* target) {target->accelerate({ 0.f, 35000.f }); }),//down
+	//	new BufferedCommand(&player, [](PhysicsObject* target) {target->accelerate({ -35000.f, 0.f }); }),//left
+	//	new BufferedCommand(&player, [](PhysicsObject* target) {target->accelerate({ 35000.f, 0.f }); })//right
+	//};
 }
 
 Player::~Player()
 {
 
 }
+
+	
