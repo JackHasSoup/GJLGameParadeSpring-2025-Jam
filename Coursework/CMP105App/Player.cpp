4871196c0f37c96e57445cf8eca0c4cfb051b942
Ross McCorkindale <ross.mccorkindale30@gmail.com>
2025-06-13 20:17:57 +0100

    created player file

---
+++ Coursework/CMP105App/Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+
+Player::Player()
+{
+	//sealTexture.loadFromFile("gfx/sealTexture.png"); //Doesn't exist yet
+	
+
+}
+
+Player::~Player()
+{
+
+}
---
+++ Coursework/CMP105App/Player.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Framework/PhysicsObject.h"
+class Player : public PhysicsObject
+{
+public:
+	Player();
+	~Player();
+private:
+};
+
