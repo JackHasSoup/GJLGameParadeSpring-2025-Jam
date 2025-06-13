3ab3e5fcda016874ba23e97fdb542e7ad044e109
Jack Sneddon <2309340@uad.ac.uk>
2025-06-13 22:53:20 +0100

    enemy now extends the creature common ancestor

--- Coursework/CMP105App/BaseEnemy.cpp
+++ Coursework/CMP105App/BaseEnemy.cpp
@@ -1,8 +1,24 @@
 #include "BaseEnemy.h"
 
-BaseEnemy::BaseEnemy(sf::Vector2f pos, sf::Vector2f size, float mass) : PhysicsObject(pos, size, mass)
+BaseEnemy::BaseEnemy(sf::Vector2f pos, sf::Vector2f size, float mass) : CreatureObject(pos, size, mass)
 {
 
+}
+
+void BaseEnemy::lightAttack()
+{
+}
+
+void BaseEnemy::heavyAttack()
+{
+}
+
+void BaseEnemy::dodge()
+{
+}
+
+void BaseEnemy::parry()
+{
 }
 
 void BaseEnemy::trackPlayer(PhysicsObject* player, std::vector<BufferedCommand*> actionBuffer, float dt) {
--- Coursework/CMP105App/BaseEnemy.h
+++ Coursework/CMP105App/BaseEnemy.h
@@ -1,14 +1,19 @@
 #pragma once
-#include "Framework/PhysicsObject.h"
+#include "CreatureObject.h"
 #include "BufferedCommand.h"
 #include <vector>
 
-class BaseEnemy : public PhysicsObject
+class BaseEnemy : public CreatureObject
 {
 public:
 	BaseEnemy() {};
 	BaseEnemy(sf::Vector2f pos, sf::Vector2f size, float mass);
 	~BaseEnemy() {};
+
+	virtual void lightAttack() override;
+	virtual void heavyAttack() override;
+	virtual void dodge() override;
+	virtual void parry() override;
 
 	void trackPlayer(PhysicsObject* player, std::vector<BufferedCommand*> actionBuffer, float dt);
 protected:
@@ -16,6 +21,6 @@ protected:
 	float speed = 200.f;
 	float attackRange = 120.f*120.f; //distance from the player at which the enemy stops approaching and starts attacking
 	int actionBufferIndex = 0; //current index of the action copy
-	static constexpr float maxCooldown = 0.75f;
+	float maxCooldown = 0.75f;
 	float cooldown = maxCooldown;
 };
