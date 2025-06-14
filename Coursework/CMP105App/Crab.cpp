6a10d76cc94147ff07c8f77d41934dfd2df0a469
AnnaliseIsFrosty <alexscheurer9@gmail.com>
2025-06-14 02:19:06 +0100

    stuff

---
+++ Coursework/CMP105App/Crab.cpp
@@ -0,0 +1,6 @@
+#include "Crab.h"
+
+Crab::Crab(sf::Vector2f pos, sf::Vector2f size, float mass) : BaseEnemy(pos, size, mass)
+{
+
+}
---
+++ Coursework/CMP105App/Crab.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "BaseEnemy.h"
+
+class Crab : public BaseEnemy
+{
+public : 
+	Crab() {};
+	Crab(sf::Vector2f pos, sf::Vector2f size, float mass);
+	~Crab() {};
+
+private : 
+
+};
+
