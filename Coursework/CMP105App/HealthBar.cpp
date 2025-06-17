bb84472c36e8279f8afde1e5bb1a828bdd18fe40
Mirren <2309167@uad.ac.uk>
2025-06-17 14:25:57 +0100

    seal now flashes red on hit

--- Coursework/CMP105App/HealthBar.cpp
+++ Coursework/CMP105App/HealthBar.cpp
@@ -12,8 +12,8 @@ HealthBar::HealthBar(sf::RenderTarget* hwnd, Player* inputPlayer, sf::Shader* in
 	window = hwnd;
 
 	player = inputPlayer;
-	maxHealth = player->getMaxHealth();
-	currentHealth = maxHealth;
+	maxHealth = player->getMaxHealth(); // This can be changed later if we want the player to gain more max health
+	currentHealth = player->getHealth();
 
 	hitEffectTimer = 0.f;
 	effectTimerMax = 0.125f;
@@ -44,6 +44,7 @@ void HealthBar::update(float dt)
 {
 	if (player->getHealth() != currentHealth) {
 		if (player->getHealth() < currentHealth) {
+			// If the player has been damaged then reset the hit effect timer
 			hitEffectTimer = effectTimerMax;
 		}
 		currentHealth = player->getHealth();
@@ -53,12 +54,19 @@ void HealthBar::update(float dt)
 	heartShader->setUniform("currentHealth", currentHealth);
 
 	if (hitEffectTimer > 0.f) {
+
+		// Red seal when hit
+		player->setFillColor(sf::Color(255, 0, 0, (hitEffectTimer/effectTimerMax)*255));
+
 		for (int i = 0; i < hearts.size(); i++) {
+		// Heart shaking up and down effect
 		hearts[i].setPosition(hearts[i].getPosition() - sf::Vector2f{ 0.f, (cos(hitEffectTimer * TAU * 400.f) * 2.f) });
 		}
 	}
 	else if(hitEffectTimer < 0.f && hitEffectTimer > -effectTimerMax) {
-		// move hearts back to where they where
+		// Reset the effects of being hit right after they're finished
+		player->setFillColor(sf::Color::White);
+
 		for (int i = 0; i < hearts.size(); i++) {
 			hearts[i].setPosition(startPos + sf::Vector2f{ i * hearts[i].getSize().x, 0.f });
 		}
@@ -67,7 +75,6 @@ void HealthBar::update(float dt)
 
 void HealthBar::render()
 {
-
 	for (int i = 0; i < hearts.size(); i++) {
 		heartShader->setUniform("index", (i * 1.f));
 		window->draw(hearts[i], heartShader);
